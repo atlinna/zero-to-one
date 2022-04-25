433e0e902359d764495ef6c185b84e69c071951c
Atlinna <53805378+atlinna@users.noreply.github.com>
2022-04-25 17:08:03 +0800

    Update forEach.js

--- HandSelf/forEach.js
+++ HandSelf/forEach.js
@@ -1,7 +1,17 @@
-### forEach
+###forEach
 
 #### 描述：
 forEach() 方法对数组的每个元素执行一次给定的函数。
+
+它的表现形式呢（调用形式）
+arr.forEach(callback(currentValue [, index [, array]])[, thisArg])
+
+**参数**
++ callback：为数组中每个元素执行的函数，该函数接收一至三个参数：
+    + currentValue：数组中正在处理的当前元素。
+    + index:数组中正在处理的当前元素的索引。
+    + array:forEach() 方法正在操作的数组。
++ thisArg：可选参数。当执行回调函数 callback 时，用作 this 的值。
 
 #### 代码实现：
 ```
